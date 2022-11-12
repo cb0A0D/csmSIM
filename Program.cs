3e6f75b74032cea13cd9cc5455db30611c3afc0a
cb0a0d@gmail.com <cb0a0d@gmail.com>
2022-11-12 15:33:21 -0500

    Main Loop Feature Added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool mainLoop = true;
+            long loopCount = 0;
+            long loopCountMax = 5;
+            while (mainLoop = true)
+            {
+                if (loopCount == loopCountMax)
+                {
+                    mainLoop = false;
+                }
+                loopCount++;
+                Console.WriteLine("Hello, World!");
+
+            }
         }
     }
 }
