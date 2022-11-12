a25d013472c79a2822f3b3d386c00fdbfbb4bebe
cb0a0d@gmail.com <cb0a0d@gmail.com>
2022-11-12 15:52:36 -0500

    Logger.cs Creation

---
+++ Logger.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Diagnostics;
+
+public class Logger
+{
+	private bool verbose;
+
+	public Logger()
+	{
+		verbose = true;
+        this.WriteLine("Logger");
+    }
+	public Logger(bool verboseFlag)
+	{
+		verbose = verboseFlag;
+        this.WriteLine("Logger");
+    }
+	private void WriteLine(string input)
+	{
+		
+		Console.WriteLine(input);
+	}
+}
